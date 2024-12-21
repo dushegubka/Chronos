d3c540a7f7bdc39ccd929ba113ff9cdda0d6a2d0
dushegubka <dushegubkatg@gmail.com>
2024-12-22 02:36:52 +0300

    Добавил забытый ApplicationDbContext.cs

---
+++ Chronos.Core/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Chronos.Core.TimeSheets;
+using LiteDB.Async;
+
+namespace Chronos.Core;
+
+public class ApplicationDbContext : IDisposable
+{
+    private const string DbName = "chronos.db";
+    private const string TimeSheetCollectionName = "timeSheets";
+    
+    private readonly ILiteDatabaseAsync _database;
+    
+    public ApplicationDbContext()
+    {
+        _database = new LiteDatabaseAsync(DbName);
+    }
+    
+    public ILiteCollectionAsync<TimeSheet> TimeSheets => _database.GetCollection<TimeSheet>(TimeSheetCollectionName);
+
+    public void Dispose()
+    {
+        _database.Dispose();
+    }
+}
